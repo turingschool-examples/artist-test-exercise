3c77c5ede25e40b7b39e257d5f8f9912748ef844
Khalid Williams <khalidwill27@gmail.com>
2019-07-19 10:33:04 -0600

    Initialize npm and install node and chai

--- artist-test.js
+++ artist-test.js
@@ -1,28 +1,28 @@
 var Song = require('./song');
 var Artist = require('./artist');
-var assert = requre('chai').assert;
+var assert = require('chai').assert;
 
 describe('Artist', function() {
-  it('should have a name', function() {
+  it.skip('should have a name', function() {
     // write test here:
 
   });
 
-  it('should be able to have a different name', function() {
+  it.skip('should be able to have a different name', function() {
     // write test here:
   })
 
-  it('should start with an empty catalogue', function() {
+  it.skip('should start with an empty catalogue', function() {
     // write test here:
 
   });
 
-  it('should be able to add songs to their catalogue', function() {
+  it.skip('should be able to add songs to their catalogue', function() {
     // write test here:
 
   });
 
-  it('should be able to return all songs of a given genre', function() {
+  it.skip('should be able to return all songs of a given genre', function() {
     // write test here:
 
   });
--- song-test.js
+++ song-test.js
@@ -2,17 +2,17 @@ var Song = require('./song');
 var assert = require('chai').assert;
 
 describe('Song' , function() {
-  it('Should have a name', function() {
+  it.skip('should have a name', function() {
     // write test here:
 
   });
 
-  it('Should have a genre', function() {
+  it.skip('should have a genre', function() {
     // write test here:
 
   });
 
-  it('Should have a length', function() {
+  it.skip('should have a length', function() {
     // write test here:
 
   })
